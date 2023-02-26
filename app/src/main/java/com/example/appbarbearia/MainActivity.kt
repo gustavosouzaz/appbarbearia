6d252b23c66b9a8d4e647879022647c119c115cb
Gustavo Souza <gustavodsouzaaa@gmail.com>
2023-02-26 00:54:41 -0300

    tentando ajeitar o botão

--- app/src/main/java/com/example/appbarbearia/MainActivity.kt
+++ app/src/main/java/com/example/appbarbearia/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.appbarbearia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+
     }
 }
